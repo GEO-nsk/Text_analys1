ff7a41e553495ae2a3ea5c9750fdee1af1adf065
Егор <geo-04@mail.ru>
2023-09-06 10:08:53 +0700

    1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 У нас росал липа. Липа стала стара. Липа стала суха. Липа упала. Пришли папа и паша. У папы пила. У паши топорик.
 """
 space = ' '
+syllable = 0
 Text = str(input())
 
 Dots = Text.count('.')
@@ -12,5 +13,7 @@ sentences = Dots + Questions + Exclamation
 
 Words = len(Text.split())
 
-for i in Text
-print(sentences, Words)
+for i in Text:
+    if i == 'а' or i == 'е' or i == 'ё' or i == 'и' or i == 'о' or i == 'у' or i == 'э' or i == 'ы' or i == 'я' or i == 'ю':
+        syllable += 1
+print(sentences, Words, syllable)
