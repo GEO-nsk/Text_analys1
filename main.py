0df361f987ee0dbdbad2628d1b8959256b5bf361
Evgeny <e.tarlo@g.nsu.ru>
2023-09-13 12:02:48 +0700

    upd

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 """
 
 from textblob import TextBlob
+from deep_translator import GoogleTranslator
+
 
 space = ' '
 syllable = 0
 Text = 'У нас росла липа. Липа стала стара. Липа стала суха. Липа упала. Пришли папа и паша. У папы пила. У паши топорик. Они распилили липу.'
+
+Text_en = GoogleTranslator(source='auto', target='en').translate(Text)
 
 Dots = Text.count('.')
 Questions = Text.count('?')
@@ -28,5 +32,31 @@ length_of_syllable = syllable/Words
 
 PRE_rus = 206.835 - 1.3*(Words/sentences) - 60.1*(syllable/Words)
 PRE_eng = 206.835 - 1.015*(Words/sentences) - 84.6*(syllable/Words)
-print(sentences, Words, syllable, length_of_sentence, length_of_syllable, PRE_rus)
 
+if PRE_rus > 80:
+    Difficulty_level = 'The text is easy to read (for younger students)'
+elif 80 >= PRE_rus > 50:
+    Difficulty_level = 'Simple text (for schoolchildren)'
+elif 50 >= PRE_rus > 25:
+    Difficulty_level = 'The text is a little hard to read (for students)'
+else:
+    Difficulty_level = 'Hard to read (for university graduates)'
+
+text_pol = TextBlob(Text_en).polarity
+
+if 1 >= text_pol > 0.1:
+    text_tone = 'Positive'
+elif 0.1 >= text_pol >= -0.1:
+    text_tone = 'Neutral'
+else:
+    text_tone = 'Negative'
+
+print('Sentences:', sentences)
+print('Words:', Words)
+print('Syllable:', syllable)
+print('Length of sentence:', length_of_sentence)
+print('Length of syllable:', length_of_syllable)
+print('Flash index:', PRE_rus)
+print('Difficulty level:', Difficulty_level)
+print('Text tone:', text_tone)
+print('Subjectivity: ', TextBlob(Text_en).subjectivity * 100, '%', sep='')
